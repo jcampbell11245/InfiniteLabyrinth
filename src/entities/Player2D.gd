d4c5194ea13e96253f902f777e00e2b474641dad
jcampbell11245 <47761573+jcampbell11245@users.noreply.github.com>
2020-10-25 13:20:31 -0400

    Implemented walking and idle animations

--- src/entities/Player2D.gd
+++ src/entities/Player2D.gd
@@ -6,6 +6,7 @@ var velocity = Vector2.ZERO
 
 func _physics_process(delta):
 	get_input()
+	animate()
 	velocity = move_and_slide(velocity)
 
 func get_input():
@@ -25,18 +26,25 @@ func get_input():
 	
 
 #Animates the player
-func animate(animation):
+func animate():
 	var animator = $AnimatedSprite;
 	
 	#Reset to idle
-	
+	if Input.is_action_just_released('right'):
+		animator.animation = "idle_right"
+	elif Input.is_action_just_released('left'):
+		animator.animation = "idle_left"
+	elif Input.is_action_just_released('down'):
+		animator.animation = "idle_down"
+	elif Input.is_action_just_released('up'):
+		animator.animation = "idle_up"
 	
 	#Movement
-	if Input.is_action_just_pressed('right'):
+	if Input.is_action_pressed('right'):
 		animator.animation = "walk_right"
-	elif Input.is_action_just_pressed('left'):
+	elif Input.is_action_pressed('left'):
 		animator.animation = "walk_left"
-	elif Input.is_action_just_pressed('down'):
+	elif Input.is_action_pressed('down'):
 		animator.animation = "walk_down"
-	elif Input.is_action_just_pressed('up'):
+	elif Input.is_action_pressed('up'):
 		animator.animation = "walk_up"
