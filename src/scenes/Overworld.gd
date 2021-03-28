81326bd71df5c3f5971bce3c882e7d23492da8ec
Reyna <iamreynasnow@gmail.com>
2021-03-27 23:20:37 -0400

    removing specific doors for world gen started

--- src/scenes/Overworld.gd
+++ src/scenes/Overworld.gd
@@ -1,19 +1,19 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var matrix = []
 var rooms = []
 var keyrooms = []
 var start_room = "res://src/scenes/StartRoom.tscn"
 var end_room = "res://src/scenes/EndRoom.tscn"
+var scenes = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	rooms()
 	keyrooms()
 	worldgen()
+	doors()
+	print(matrix)
 	pass # Replace with function body.
 
 func rooms():
@@ -51,31 +51,30 @@ func keyrooms():
 #		print("Got a list of "+str(numScenes)+" files")
 
 func worldgen():
-	var key_x = []
-	for i in range(3):
-		var rand = int(rand_range(1, 7))
-		while (rand in key_x):
-			rand = int(rand_range(1, 7))
-		key_x.append(rand)
-	
 	var key_y = []
 	for i in range(3):
-		var rand = int(rand_range(0, 5))
+		var rand = int(rand_range(1, 7))
 		while (rand in key_y):
-			rand = int(rand_range(0, 5))
+			rand = int(rand_range(1, 7))
 		key_y.append(rand)
 	
-	var matrix = []
-	for x in range(8):
+	var key_x = []
+	for i in range(3):
+		var rand = int(rand_range(0, 5))
+		while (rand in key_x):
+			rand = int(rand_range(0, 5))
+		key_x.append(rand)
+	
+	for x in range(5):
 		matrix.append([])
-		for y in range(5):
-			if (x == 0):
-				if (y == 2):
+		for y in range(8):
+			if (y == 0):
+				if (x == 2):
 					matrix[x].append(start_room)
 				else:
 					matrix[x].append('Null')
-			elif(x == 7):
-				if (y == 2):
+			elif(y == 7):
+				if (x == 2):
 					matrix[x].append(end_room)
 				else:
 					matrix[x].append('Null')
@@ -91,6 +90,15 @@ func worldgen():
 
 func randomscene(var scenes):
 	return scenes[int(rand_range(0, scenes.size()))]
+
+func doors():
+	for x in range(5):
+		if(x == 2):
+			continue
+		for y in range(1, 7):
+			if (x == 0):
+				var node = scenes.get(matrix[x][y].substr(-7, 2)).instance
+#continue work on doors, ^ will get the particular preloaded scene(all rooms will be preloaded at top) and instance it
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
