42b8a9539ad57dae2c09a70e6494d50c0d13f08a
Jonathan Campbell <47761573+jcampbell11245@users.noreply.github.com>
2021-12-30 19:57:01 -0500

    Made it so player cannot unpause after dying or completing the game

--- src/hud/Pause.gd
+++ src/hud/Pause.gd
@@ -1,10 +1,14 @@
 extends Control
 
+var pause_state = false
+
 func _input(event):
-	if event.is_action_pressed("pause"):
+	if(event.is_action_pressed("pause")):
 		pause()
 
 func pause():
-	var new_pause_state = !get_tree().paused
-	get_tree().paused = new_pause_state
-	visible = new_pause_state
+	if(pause_state || !get_tree().paused):
+		var new_pause_state = !get_tree().paused
+		get_tree().paused = new_pause_state
+		visible = new_pause_state
+		pause_state = new_pause_state
